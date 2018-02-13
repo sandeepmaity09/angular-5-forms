395a88df3a8b47f8e20ddb9f5b27306d36f5fdba
sandeepmaity09 <sandeepmaity09@gmail.com>
2018-02-13 19:48:40 +0530

    forms angualr

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+@Component({
+	selector: 'app-hero-form',
+	templateUrl: './hero-form.component.html',
+	styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	values = '';
+
+	powers = ['Really Smart','Super Flexible','Super Hot','Weather Changer'];
+
+	model = new Hero(18,'Dr IQ',this.powers[0],'Chuck Overstreet');
+
+	submitted = false;
+
+	onSubmit(){
+		this.submitted = true;
+	}
+
+	onKeyup(event:any){
+		console.log(event);
+		this.values += event.target.value + ' | ';
+	}
+
+  // TODO: Remove this when we're done
+  get diagnostic(){
+  	return JSON.stringify(this.model);
+  }
+
+}
